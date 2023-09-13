6da1a8a8cfc702c44a6d078a0add46d6b65bf138
Wairimu <charitykabuu21@gmail.com>
2023-09-13 23:29:57 +0300

    Update 1-multi_languages.js

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -3,5 +3,5 @@ const myVar = "C is fun";
 const value1 = "Python is cool";
 const value2 = "Javascript is amazing";
 
-// Use console.log() to print the values of myVar
+// Use console.log() to print the values of myVar myVar, value1, and value2 on separate lines
 console.log(myVar + "\n" + value1 + "\n" + value2);
