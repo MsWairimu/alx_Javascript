1d928dc6cc06bf25cbbc08d6e4ec44f2ac2ee5c1
Wairimu <charitykabuu21@gmail.com>
2023-09-13 23:30:59 +0300

    Update 1-multi_languages.js

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
-// Declare a contant variable called myVar
+// Declare a constant variable called myVar
 const myVar = "C is fun";
 const value1 = "Python is cool";
-const value2 = "Javascript is amazing";
+const value2 = "JavaScript is amazing";
 
-// Use console.log() to print the values of myVar myVar, value1, and value2 on separate lines
+// Use console.log() to print the values of myVar, value1, and value2 on separate lines
 console.log(myVar + "\n" + value1 + "\n" + value2);
