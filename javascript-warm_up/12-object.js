9e3660fe27985bdefbaea31dc5fc91c23c456822
Wairimu <charitykabuu21@gmail.com>
2023-09-14 00:11:24 +0300

    Update 12-object.js

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,11 +1,12 @@
 // Declare a constant variable called myObject
 const myObject = {
   type: 'object',
-  value: 12
-};
+  value: 12;
+
 
 // Change the value property to 89
-myObject.value = 89
+myObject.value = 89;
 
 // Print the updated object
 console.log(myObject);
+};
