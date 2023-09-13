91cd2761a73efbca005c3a02d8fe528e4e7fadef
Wairimu <charitykabuu21@gmail.com>
2023-09-13 23:22:05 +0300

    Create 1-multi_languages.js

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+// Declare a contant variable called myVar with the value "C is fun"
+const myVar = "C is fun";
+const value1 = "Python is cool";
+const value2 = "Javascript is amazing";
+
+// Use console.log() to print the value of myVar
+console.log(myVar + " " value1 + " " value2);
