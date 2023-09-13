7ff41f4d6b9188aa3423870436d7dd5431a722ea
Wairimu <charitykabuu21@gmail.com>
2023-09-13 23:44:03 +0300

    Create 6-multi_languages_loop.js

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,14 @@
+// Declare an array of values
+const values = ["C is fun", "Python is cool", "Javascript is amazing"];
+
+// Use a while loop to print each values on separate lines
+let i = 0;
+while (i < values.length) {
+  console.log(values[i]);
+  i++;
+}
+
+// Use a for loop to achieve the same result
+for (let j = 0; j < values.length; j++) {
+  console.log(values[j]);
+}
