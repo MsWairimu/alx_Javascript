b2cdd11d4be77d0026c9a5847e1f15df106ae0d0
Wairimu <charitykabuu21@gmail.com>
2023-09-14 00:06:32 +0300

    Update 13-add.js

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,6 +1,9 @@
-// Define a function named "add" that takes 2 integers as parameters and returns their addition.
+// Define the add function
 function add(a, b) {
   return a + b;
 }
-const result = add(5, 7);
-console.log(result); // This will print 12 to the console
+
+// Export the add function
+module.exports = {
+  add: add
+};
