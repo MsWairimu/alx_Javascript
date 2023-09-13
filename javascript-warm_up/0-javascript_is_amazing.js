192aebee323fefd83b0b01c5193279a0dae28ce9
Wairimu <charitykabuu21@gmail.com>
2023-09-13 23:10:03 +0300

    Update 0-javascript_is_amazing.js

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,2 +1,5 @@
-const myVar = "Javascript is amazing";
-console.log("myVar");
+// Step 1: Declare a constant variable called myVar with the value "JavaScript is amazing".
+const myVar = "JavaScript is amazing";
+
+// Step 2: Use console.log() to print the value of myVar.
+console.log(myVar);
