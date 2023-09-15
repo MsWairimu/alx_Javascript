30fceb65d2c58cb1b1e710f6afdf9a1dde8ed104
Wairimu <charitykabuu21@gmail.com>
2023-09-16 00:28:47 +0300

    Update 102-add_me_maybe.js

--- javascript-warm_up/102-add_me_maybe.js
+++ javascript-warm_up/102-add_me_maybe.js
@@ -1,8 +1,8 @@
-function incrementAndCall(number, theFunction) {
+imports.addMeMaybe = (number, theFunction) {
   if (typeof number === 'number' && typeof theFunction === 'function') {
     number++; // Increment the number
     theFunction(number); // Call the provided function with the incremented number as an argument
   } else {
     console.error('Invalid arguments. The first argument must be a number, and the second argument must be a function.');
   }
-}
+};
