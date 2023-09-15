7d71ccfe7da1cd56ccdc9dcfe40fa9f5e2df83ab
Wairimu <charitykabuu21@gmail.com>
2023-09-16 00:48:25 +0300

    Update 103-object_fct.js

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -2,13 +2,20 @@
 
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: function() {
-    this.value++;
-  }
+  value: 12
 };
 
 console.log(myObject);
+
+function incr() {
+  if (typeof this.value === 'number') {
+    this.value++;
+  } else {
+    console.error('Value is not a number.');
+  }
+}
+
+myObject.incr = incr;
 
 myObject.incr();
 console.log(myObject);
