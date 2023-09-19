a9ed1c282d8410d47282b2fd38fff29b04e4ffe0
Wairimu <charitykabuu21@gmail.com>
2023-09-19 23:48:30 +0300

    Update 0-rectangle.js

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,7 +1,4 @@
-module.exports = class Rectangle{
+module.exports = class Rectangle {
     // Constructor and class definition go here
 }
 
-const r1 = new Rectangle();
-console.log(r1);
-console.log(r1.constructor);
