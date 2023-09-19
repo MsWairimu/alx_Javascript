e76ced3a752be84ab09d305f82c9f8396f20ae77
Wairimu <charitykabuu21@gmail.com>
2023-09-19 23:19:51 +0300

    Update 1-rectangle.js

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -5,22 +5,6 @@ public class Rectangle {
     public Rectangle(int w, int h) {
         this.width = w;
         this.height = h;
-
-const Rectangle = require('./1-rectangle');
-
-const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
-
-const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.width);
-cosle.log(r2.height);
         }
 }
 
-const r3 = new Rectangle(2);
-console.log(r3):
-console.log(r3.width);
-console.log(r3.height);
