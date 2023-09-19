10ccf457b287f36228858309d32dc382675d40d8
Wairimu <charitykabuu21@gmail.com>
2023-09-19 23:34:38 +0300

    Update 1-rectangle.js

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,41 +1,6 @@
-#!/usr/bin/node
-
-// Define the Rectangle class using class notation
-class Rectangle {
+module.ecports = class Rectangle {
     constructor(w = 0, h = 0) {
         this.width = w;
         this.height = h;
     }
-}
-
-const r1 = new Rectangle();
-console.log(r1);
-console.log(r1.width); // 0
-console.log(r1.height); // 0
-
-const Rectangle = require('./1-rectangle');
-
-const r1 = new Rectangle(3, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
-
-const r1 = new Rectangle(3, -3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
-
-const r1 = new Rectangle(3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
-
-const r1 = new Rectangle(undefined, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
-
-const r1 = new Rectangle();
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
+};
