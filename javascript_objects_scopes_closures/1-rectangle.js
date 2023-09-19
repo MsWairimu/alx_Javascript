3273f434188465dc26b077f11f6a5c99d15a3109
Wairimu <charitykabuu21@gmail.com>
2023-09-19 23:27:50 +0300

    Update 1-rectangle.js

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,36 @@
-public class Rectangle {
-    private int width;
-    private int height;
+#!/usr/bin/node
 
-    public Rectangle(int w, int h) {
+// Define the Rectangle class using class notation
+class Rectangle {
+    constructor(w, h) {
         this.width = w;
         this.height = h;
-        }
+    }
 }
 
+const Rectangle = require('./1-rectangle');
+
+const r1 = new Rectangle(3, 3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
+
+const r1 = new Rectangle(3, -3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
+
+const r1 = new Rectangle(3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
+
+const r1 = new Rectangle(undefined, 3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
+
+const r1 = new Rectangle();
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
