3b34dfda33ab245ce502a312eae0a7d232dbb7dd
Wairimu <charitykabuu21@gmail.com>
2023-09-21 00:11:10 +0300

    Update 5-square.js

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,4 @@
-const Rectangle = require('./4-Rectangle'){
-}
+const Rectangle = require('./4-Rectangle');
 class Square extends Rectangle {
     constructor(size) {
         // Call the constructor of the parent class (Rectangle) using super()
