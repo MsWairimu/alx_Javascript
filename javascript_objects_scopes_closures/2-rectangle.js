a752fc9769fd5ebc31e47d880c386d7a6dd56ceb
Wairimu <charitykabuu21@gmail.com>
2023-09-20 22:47:30 +0300

    Update 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
-module.exports = class Rectangle{
-class Rectangle {
-    constructor(w <= 0 || h <= 0){
-        this.width = w;
-        this.height = h;
-    }
-};
+module.exports = class Rectangle {
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+            // If w or h is not a positive integer or is 0, create an empty object
+            this.width = undefined;
+            this.height = undefined;
+        } else {
+            // Initialize the instance attributes width and height
+            this.width = w;
+            this.height = h;
