37ac109954300c708444dcf1bd7e362b2e752b1c
Wairimu <charitykabuu21@gmail.com>
2023-09-20 23:49:31 +0300

    Update 4-rectangle.js

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -48,3 +48,5 @@ double() {
 //Initialize the instance attribute height with the value of h
 //If w or h is equal to 0 or not a positive integer, create an empty object
 //Create an instance method called print() that prints the rectangle using the character X
+//Create an instance method called rotate() that exchanges the width and the height of the rectangle
+//Create an instance method called double() that multiples the width and the height of the rectangle by 2
