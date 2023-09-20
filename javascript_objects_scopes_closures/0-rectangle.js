b782e50c4462db087a9d7001dbdd40b8f3de03e2
Wairimu <charitykabuu21@gmail.com>
2023-09-20 23:02:42 +0300

    Update 0-rectangle.js

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,4 +1,5 @@
 module.exports = class Rectangle {
     // Constructor and class definition go here
 }
-
+//Write an empty class Rectangle that defines a rectangle:
+//You must use the class notation for defining your class
