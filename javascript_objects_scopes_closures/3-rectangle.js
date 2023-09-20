a1e6b6a2f8e566ec714698f4e310fa98ee32ef1c
Wairimu <charitykabuu21@gmail.com>
2023-09-20 23:30:15 +0300

    Update 3-rectangle.js

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,27 +1,29 @@
 #!/usr/bin/node
+
 module.exports = class Rectangle {
     constructor(w, h) {
-        if(w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number' || !Number.isInteger(w) || !Number.isInteger(h)){
+        if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number' || !Number.isInteger(w) || !Number.isInteger(h)) {
             // If w or h is not a positive integer or is 0, create an empty object
-           return 'Rectangle {}';
+            return 'Rectangle {}';
         } else {
             // Initialize the instance attributes width and height
             this.width = w;
             this.height = h;
         }
-print() {
-    for (let i = 0; i < this.height; i++){
-        let row = '';
-        for (let j = 0; j < this.width; j++) {
-                 row += 'X';
-             }
-                console.log(row)
+    }
+
+    print() {
+        if (typeof this.width === 'undefined' || typeof this.height === 'undefined') {
+            console.log('Invalid Rectangle');
+        } else {
+            // Print the rectangle with 'X' characters
+            for (let i = 0; i < this.height; i++) {
+                let row = '';
+                for (let j = 0; j < this.width; j++) {
+                    row += 'X';
+                }
+                console.log(row);
             }
         }
     }
-
-
-
-
-
-
+}
