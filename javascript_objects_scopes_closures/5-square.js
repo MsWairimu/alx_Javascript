c4b2ebf9463998935912744c6f72844a9542039b
Wairimu <charitykabuu21@gmail.com>
2023-09-21 00:00:03 +0300

    Update 5-square.js

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,4 +1,4 @@
-module.exports = class Rectangle{
+module.exports = class Rectangle {
     constructor (w, h) {
       this.width = w; 
       this.height = h;
@@ -8,6 +8,6 @@ module.exports = class Rectangle{
 class Square extends Rectangle {
   constructor(size) {
     // Call the constructor of the parent2class (Rectangle) using super()
-  super(size,2size);
+  super(size, size);
   }
 }
