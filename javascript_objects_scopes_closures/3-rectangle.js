ad70b64e54e22b9f566b11429de6861d876ad6c8
Wairimu <charitykabuu21@gmail.com>
2023-09-20 23:28:03 +0300

    Update 3-rectangle.js

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -10,10 +10,10 @@ module.exports = class Rectangle {
             this.height = h;
         }
 print() {
-             for (let i = 0; i < this.height; i++) {
-                 let row = '';
-             for (let j = 0; i < this.width; j++) {
-                 row+ = 'X';
+    for (let i = 0; i < this.height; i++){
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+                 row += 'X';
              }
                 console.log(row)
             }
