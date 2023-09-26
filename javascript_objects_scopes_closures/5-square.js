c8ff893ad7a8abe21140b5e574e66dd5ee9199ab
Wairimu <charitykabuu21@gmail.com>
2023-09-26 10:33:29 +0300

    Update 5-square.js

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,15 +1,10 @@
-#!/usr/bin/node
-
-module.exports = class Rectangle {
-
-class Square extends Rectangle {
-  constructor(size) {
-    // Call the constructor of the Rectangle class using super()
-    super(size, size);
-  }
-}
-
-// Usage example
-const square = new Square(5);
-console.log("Square Area:", square.width, "x", square.height);
-square.print();
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number' || !Number.isInteger(w) || !Number.isInteger(h)) {
+            // If w or h is not a positive integer or is 0, create an empty object
+            return 'Rectangle {}';
+        } else {
+            // Initialize the instance attributes width and height
+            this.width = w;
+            this.height = h;
+        }
+    }
