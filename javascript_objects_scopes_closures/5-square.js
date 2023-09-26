b060156ec4e9bfe6092d8d511102c1fc3496cfd5
Wairimu <charitykabuu21@gmail.com>
2023-09-26 10:43:09 +0300

    Update 5-square.js

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,47 +1,7 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number' || !Number.isInteger(w) || !Number.isInteger(h)) {
-      // If w or h is not a positive integer or is 0, create an empty object
-      return 'Rectangle {}';
-    } else {
-      // Initialize the instance attributes width and height
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  rotate() {
-    if (typeof this.width !== 'undefined' && typeof this.height !== 'undefined') {
-      // Swap width and height
-      [this.width, this.height] = [this.height, this.width];
-    }
-  }
-
-  double() {
-    if (typeof this.width !== 'undefined' && typeof this.height !== 'undefined') {
-      // Multiply width and height by 2
-      this.width *= 2;
-      this.height *= 2;
-    }
-  }
-
-  print() {
-    if (typeof this.width === 'undefined' || typeof this.height === 'undefined') {
-      console.log('Invalid Rectangle');
-    } else {
-      // Print the rectangle with 'X' characters
-      for (let i = 0; i < this.height; i++) {
-        let row = '';
-        for (let j = 0; j < this.width; j++) {
-          row += 'X';
-        }
-        console.log(row);
-      }
-    }
-  }
-}
+// Import the Rectangle class from the 4-rectangle.js module
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor(size) {
