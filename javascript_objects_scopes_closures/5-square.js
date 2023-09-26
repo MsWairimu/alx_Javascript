752415289269a6e3c136278d92d0d21990d80dfa
Wairimu <charitykabuu21@gmail.com>
2023-09-26 10:27:59 +0300

    Update 5-square.js

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,11 +1,25 @@
-#!/usr/bin/node
-const Rectangle = require('./4-Rectangle');
+// Import the Rectangle class from 4-rectangle.js if it's defined in a separate file
+// Make sure to provide the correct path to the Rectangle class.
 
-class Square extends Rectangle {
-    constructor(size) {
-        // Call the constructor of the Rectangle class using super()
-        super(size, size);
-    }
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  area() {
+    return this.width * this.height;
+  }
 }
 
-module.exports = Square;
+// Square class inherits from Rectangle
+class Square extends Rectangle {
+  constructor(size) {
+    // Call the constructor of the Rectangle class using super()
+    super(size, size);
+  }
+}
+
+// Usage example
+const square = new Square(5);
+console.log("Square Area:", square.area()); // Output: Square Area: 25
